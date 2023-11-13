407b3ebfe267465517e9d290acc88ae419461d10
liu-ziting <liuziting2014@163.com>
2023-11-13 14:18:15 +0800

    修改bug

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,10 @@ import { createPinia } from 'pinia'
 import 'nprogress/nprogress.css'
 import App from './App.vue'
 import router from './router'
-import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(autoAnimatePlugin)
 
 app.mount('#app')
